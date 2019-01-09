c4546b4bff400874c73bd3ca290f4e5cc7756061
Gilmar J. A. Goulart <gilmar.g@outlook.com>
2019-01-09 14:46:25 -0200

    Commit inicial

--- App01_ConsultarCEP/App01_ConsultarCEP/MainPage.xaml.cs
+++ App01_ConsultarCEP/App01_ConsultarCEP/MainPage.xaml.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using App01_ConsultarCEP.Servico.Modelo;
+using App01_ConsultarCEP.Servico;
 
 namespace App01_ConsultarCEP
 {
@@ -12,6 +14,66 @@ namespace App01_ConsultarCEP
         public MainPage()
         {
             InitializeComponent();
+
+            btnBuscarCep.Clicked += BuscarCEP;
+        }
+
+        private void BuscarCEP(object sender, EventArgs args)
+        {
+            // TODO - lógica do programa
+
+            // TODO - Validações
+
+
+            // TODO - Buscar
+            string cep = txtCep.Text.Trim();
+            if (IsValidCEP(cep))
+            {
+                try
+                {
+                    Endereco endereco = ViaCEPServico.BuscarEnderecoViaCEP(cep);
+
+                    txtResultado.Text = string.Format("{0}, {1}, {2} - {3}", endereco.logradouro, endereco.bairro, endereco.localidade, endereco.uf);
+                }
+                catch (Exception e)
+                {
+                    txtResultado.Text = "ERRO";
+                    DisplayAlert("Erro ao consultar CEP", e.Message, "Ok");
+                }
+            }
+            else
+            {
+                txtResultado.Text = "ERRO";
+            }
+        }
+
+        private bool IsValidCEP(string cep)
+        {
+            bool retorno = true;
+            string msgErr = "";
+            if (cep.Length != 8)
+            {
+                // ERRO
+                msgErr += "CEP inválido. Deve conter 8 dígitos.";
+                retorno = false; ;
+            }
+            int cepNum = 0;
+            if (!int.TryParse(cep, out cepNum))
+            {
+                //ERRO
+                if (msgErr.Length > 1)
+                {
+                    msgErr += "\n";
+                }
+                msgErr += "CEP inválido. Deve conter apenas números.";
+                retorno = false; ;
+            }
+
+            if (!retorno)
+            {
+                DisplayAlert("Erro", msgErr, "Ok");
+            }
+            return retorno;
         }
     }
 }
